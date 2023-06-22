26ac943e1c676cd4045a5df3b93898d6bd2045e3
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-22 15:01:53 -0300

    Entendendo o this

--- objetoLiteral.js
+++ objetoLiteral.js
@@ -4,8 +4,21 @@ const user = {
     nascimento: "2021/01/01",
     role: "admin",
     ativo: true,
-    exibirInfo: function() {
+    exibirInfos: function () {
         console.log(this.nome, this.email)
     }
 }
 
+// user.exibirInfos();
+// const exibir = user.exibirInfos;
+// exibir();
+
+const exibir = function () {
+    console.log(this.nome);
+}
+
+const exibirNome = exibir.bind(user);  // bind - prender a execucao de uma funcao
+
+exibirNome()
+exibir()
+// A diferenca e que se passar o user como parametro em bind(), o this. consegue encontrar a chave
