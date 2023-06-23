adb3000c1b5f7b5f081ac50089396a42be4faa3e
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-23 12:30:21 -0300

    Metodos privados

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import Admin from "./Admin.js";
 import Docente from "./Docente.js";
 
 const novoUser = new User('Mariana', 'm@m.com', '2021-01-01')
+const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
+
 console.log(novoUser.exibirInfos())
+console.log(novoAdmin.exibirInfos())
+
+// console.log(novoUser.#montaObjUser())
 
 // '#nome' e 'nome' nao sao o mesmo atributo
 // novoUser.#nome = 'Marcia';
