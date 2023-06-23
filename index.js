a7cbd7404c05e46ac0f0c2ce6806d587eb1736b3
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-06-23 13:49:55 -0300

    Getters

--- index.js
+++ index.js
@@ -2,14 +2,8 @@ import User from "./User.js";
 import Admin from "./Admin.js";
 import Docente from "./Docente.js";
 
-const novoUser = new User('Mariana', 'm@m.com', '2021-01-01')
+// const novoUser = new User('Mariana', 'm@m.com', '2021-01-01')
 const novoAdmin = new Admin('Rodrigo', 'r@r.com', '2021-01-01')
 
-console.log(novoUser.exibirInfos())
-console.log(novoAdmin.exibirInfos())
-
-// console.log(novoUser.#montaObjUser())
-
-// '#nome' e 'nome' nao sao o mesmo atributo
-// novoUser.#nome = 'Marcia';
-// console.log(novoUser.nome)
+// console.log(novoUser.exibirInfos())
+console.log(novoAdmin.nome)
